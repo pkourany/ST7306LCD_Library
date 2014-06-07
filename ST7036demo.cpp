4bc38793aae3a654f4a03e06c9d8b8e4140d41ac
Paul Kourany <pkourany@gmail.com>
2014-06-06 23:46:32 -0400

    Spark Port

---
+++ ST7036demo.cpp
@@ -0,0 +1,17 @@
+#include "ST7036.h"
+
+ST7036 lcd = ST7036 ( 2, 20, 0x78 ); //2 lines, 20 characters, 0x78 address
+
+void setup ()
+{
+   lcd.init ();
+   lcd.clear ();
+   lcd.setCursor ( 0, 0 );
+   lcd.print ("Temperature:");
+}
+
+void loop ()
+{
+
+
+}
